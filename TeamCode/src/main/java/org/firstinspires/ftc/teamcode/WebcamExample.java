7b0c899e58ec757dc1584e2dc96301702cc77b51
mogggit <moggit2023@gmail.com>
2022-01-04 18:32:06 +0800

    Added and tested TensorFlowObjectDetection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamExample.java
@@ -16,7 +16,7 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
-@TeleOp
+@TeleOp(name = "WebcamExample")
 public class WebcamExample extends LinearOpMode
 {
     OpenCvWebcam webcam;
@@ -66,20 +66,7 @@ public class WebcamExample extends LinearOpMode
             public void onOpened()
             {
                 /*
-                 * Tell the webcam to start streaming images to us! Note that you must make sure
-                 * the resolution you specify is supported by the camera. If it is not, an exception
-                 * will be thrown.
-                 *
-                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
-                 * supports streaming from the webcam in the uncompressed YUV image format. This means
-                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
-                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
-                 *
-                 * Also, we specify the rotation that the webcam is used in. This is so that the image
-                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
-                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
-                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
-                 * away from the user.
+                 * Tell the webcam to start streaming images and specify the rotation that the webcam is used in.
                  */
                 webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
             }
@@ -207,7 +194,17 @@ public class WebcamExample extends LinearOpMode
                     new Point(
                             input.cols()*(3f/4f),
                             input.rows()*(3f/4f)),
-                    new Scalar(0, 255, 0), 4);
+                    new Scalar(0, 255, 0), 3);
+
+            Imgproc.line(input,
+                    new Point(
+                            input.cols()/4 + 100,
+                            input.rows()/4),
+                    new Point(
+                            input.cols()/4 + 100,
+                            input.rows()*(3f/4f)),
+                    new Scalar(255, 0, 0), 3);
+
 
             /**
              * NOTE: to see how to get data from your pipeline to your OpMode as well as how
