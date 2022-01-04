19a0f43c683548d81db641f6391fdff3555cf26b
mogggit <moggit2023@gmail.com>
2022-01-04 12:35:13 +0800

    Implemented FTC Dashboard to WebcamExample

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WebcamExample.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
@@ -34,6 +36,9 @@ public class WebcamExample extends LinearOpMode
          */
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        Telemetry dashboardTelemetry = dashboard.getTelemetry();
+        dashboard.startCameraStream(webcam, 30);
 
         // OR...  Do Not Activate the Camera Monitor View
         //webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"));
@@ -108,6 +113,10 @@ public class WebcamExample extends LinearOpMode
             telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
             telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
             telemetry.update();
+
+            dashboardTelemetry.addData("Frame Count", webcam.getFrameCount());
+            dashboardTelemetry.addData("FPS", String.format("%.2f", webcam.getFps()));
+            dashboardTelemetry.update();
 
             /*
              * NOTE: stopping the stream from the camera early (before the end of the OpMode
