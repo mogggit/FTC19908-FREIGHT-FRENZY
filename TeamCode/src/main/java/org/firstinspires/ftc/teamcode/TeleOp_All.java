09a2309acc20e452a44d326954a7383280e87ed0
Moggit <74050723+mogggit@users.noreply.github.com>
2022-03-06 11:53:50 +0800

    Added comments that explain TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_All.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_All.java
@@ -7,10 +7,9 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-@TeleOp(name = "TeleOp All")
+@TeleOp(name = "TeleOp All") // name of this program shown in ds
 public class TeleOp_All extends LinearOpMode {
 
     private Drivetrain drivetrain;
@@ -20,10 +19,13 @@ public class TeleOp_All extends LinearOpMode {
     private Servo grab;
     private TouchSensor touch;
 
+    // combines dashboard telemetry and ds telemetry
     private MultipleTelemetry tel = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
     @Override
     public void runOpMode() {
+        
+        // instantiate every part of the robot
         drivetrain = new Drivetrain(
                 hardwareMap.dcMotor.get("M1"), // top left wheel
                 hardwareMap.dcMotor.get("M2"), // bottom left wheel
@@ -33,9 +35,10 @@ public class TeleOp_All extends LinearOpMode {
         pivot = hardwareMap.dcMotor.get("pivot"); // pivot
         slide = hardwareMap.dcMotor.get("slide"); // linear slide
         spinner = hardwareMap.dcMotor.get("spinner"); // spinner
-        grab = hardwareMap.servo.get("grab"); // grab
-        touch = hardwareMap.touchSensor.get("touch");
+        grab = hardwareMap.servo.get("grab"); // grab servo
+        touch = hardwareMap.touchSensor.get("touch"); // touch sensor for slide
 
+        // set pivot, slide, spinner, and drivetrain to teleOp mode
         pivot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         pivot.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -46,25 +49,25 @@ public class TeleOp_All extends LinearOpMode {
         spinner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         drivetrain.driveMode();
 
-        int grabValue = -140;
-        int balanceValue = 0;
-        int topValue = 1000;
-        int highValue = 1200;
-        int lowValue = -90;
-        double pivotCorrection = 0;
+        // encoder values for different pivot positionsd
+        int grabValue = -140; // almost touch the ground to grab game elements
+        int balanceValue = 0; // horinzontal
+        int topValue = 1000; // verticle
+        int highValue = 1200; // high level
+        int lowValue = -90; // low level
+        double pivotCorrection = 0; // manual correction
 
+        // wait for start button to be pressed
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                /* Mecanum Wheels */
+                // mecanum Wheels
                 drivetrain.drive(gamepad2);
 
-                //-145
-                //1300
-                tel.addData("Gamepad1.left_stick_y", gamepad1.left_stick_y);
-                tel.addData("Gamepad1.right_stick_y", gamepad1.right_stick_y);
+                // spinner for duck
                 spinner.setPower(gamepad1.right_trigger);
-
+                
+                // grab servo
                 if (gamepad1.right_bumper) {
                     grab.setPosition(1);
                 }
@@ -75,6 +78,7 @@ public class TeleOp_All extends LinearOpMode {
                     grab.setPosition(0.42);
                 }
 
+                // slide control with fail-safe using touch sensor
                 if (slide.getCurrentPosition() < -4200 && gamepad1.left_stick_y < 0) {
                     slide.setPower(0);
                 }
@@ -85,6 +89,7 @@ public class TeleOp_All extends LinearOpMode {
                     slide.setPower(gamepad1.left_stick_y);
                 }
 
+                // pivot
                 if (gamepad1.b) {
                     pivotStay(0.3, grabValue);
                 }
@@ -94,10 +99,14 @@ public class TeleOp_All extends LinearOpMode {
                 else if (gamepad1.y) {
                     pivotStay(0.3, topValue);
                 }
+                else if (gamepad1.a) {
+                    pivotStay(0.3, lowValue);
+                }
                 else {
                     pivotStay(0.3, balanceValue);
                 }
 
+                // driver 1 manual correction for pivot
                 if (gamepad1.right_stick_y != 0) {
                     pivotCorrection = -gamepad1.right_stick_y;
                     grabValue += pivotCorrection;
@@ -107,6 +116,7 @@ public class TeleOp_All extends LinearOpMode {
                     lowValue += pivotCorrection;
                 }
 
+                // driver 2 manual correction for pivot
                 if (gamepad2.right_stick_y != 0) {
                     pivotCorrection = -gamepad1.right_stick_y;
                     grabValue += pivotCorrection;
@@ -116,10 +126,9 @@ public class TeleOp_All extends LinearOpMode {
                     lowValue += pivotCorrection;
                 }
 
-                if (gamepad1.a) {
-                    pivotStay(0.3, lowValue);
-                }
-
+                // telemetry data to dashboard and ds
+                tel.addData("Gamepad1.left_stick_y", gamepad1.left_stick_y);
+                tel.addData("Gamepad1.right_stick_y", gamepad1.right_stick_y);
                 tel.addData("M1 Encoder", drivetrain.getEncoder("m1"));
                 tel.addData("M2 Encoder", drivetrain.getEncoder("m2"));
                 tel.addData("M3 Encoder", drivetrain.getEncoder("m3"));
@@ -133,6 +142,7 @@ public class TeleOp_All extends LinearOpMode {
         }
     }
 
+    // prevent pivot from tipping
     public void pivotStay(double power, int p1){
         double pivotPower = power * ((p1 - pivot.getCurrentPosition()) / 100.0);
         if (pivotPower > 0.3) {
